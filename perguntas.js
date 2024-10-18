dce476769e7ffe5c0ccb7979ca3961eca54fb74b
dullpie <00001124047803sp@al.educacao.sp.gov.br>
2024-10-18 08:09:05 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'rpg do sotiblec',
+    'Quem é Kian?',
+    'Para o cego, é a cegueira. Para o faminto, é a fome. Para o sedento, é a sede. Para o morto, é o fim. Para o enfermo, é a doença. Para o prisioneiro, é a prisão.'
+)
+
+criaCartao(
+    'Lupi',
+    'O que é Enigma do Medo?',
+    'ERROR 404 - Vis Unita Fortior'
+)
+
+criaCartao(
+    'Tenha modos!',
+    'Como se diz de nada em inglês?',
+    'https://www.youtube.com/watch?v=79DijItQXMM&pp=ygUVbW9hbmEgeW91J3JlIHdlbGNvbWUg'
+)
+
+criaCartao(
+    'Anime',
+    'PERGUNTA ELIMINATÓRIA: Qual o melhor anime de todos os tempos?',
+    'konosuba (KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK)'
+)
